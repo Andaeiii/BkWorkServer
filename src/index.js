3eac7e04067e03017b676f197909eb983999576b
Ande Caleb <andaeiii@aol.com>
2020-12-30 14:32:14 +0100

    setting up CORs on express

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import path from 'path';
 
 
 const app = express();
 
+app.options('*', cors());
+
+app.post('/api/auth', (req, res)=>{
+    res.status(400).json({errors:{global: "invalid credentials"}})
+})
+
 app.get('/*', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html')); 
 });
 
 app.listen(8080, ()=>console.log('running on localhost 8080.'));
